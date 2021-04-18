ff2d6bbff80bd5129ba927aaffb6fe8a8025f0f5
DravenRedgrave <77494310+DravenRedgrave@users.noreply.github.com>
2021-04-18 20:53:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const menuToggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('nav');
+  menuToggle.addEventListener('click', () => {
+      if (nav.className != 'active') {nav.className = 'active';}
+      else {nav.className = '';}
+  });
+
+
+  $(document).ready(function(){
+    $('.slider-content').slick({
+      arrows:false,
+      slidesToShow:1,
+      infinite:true,
+      autoplay:true,
+      pauseOnFocus:true,
+      adaptiveHeight:true,
+      pauseOnHover:true,
+      swipe:true,
+      TouchMove:true,
+      responsive:[
+        {
+          breakpoint: 768,
+          settings:{
+              arrows:false,
+            slidesToShow:1,
+          }
+        }, {
+          breakpoint: 922,
+          settings:{
+            arrows:false,
+            slidesToShow:1,
+          }
+        }, {
+          breakpoint: 480,
+          settings:{
+            arrows:false,
+            slidesToShow:1
+          }
+        }
+      ]
+    });
+      });
